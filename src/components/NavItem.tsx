7f719d6fa63a032c9ae63f4279a97e5bf0fdb5c5
nkc930604@gmail.com <nkc930604@gmail.com>
2024-01-31 17:19:48 +0900

    first commit

---
+++ src/components/NavItem.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import React from "react";
+
+interface NavItemProps {
+  mobile?: boolean;
+}
+
+const NavItem = (props: NavItemProps) => {
+  const { mobile } = props;
+  return (
+    <ul
+      className={`text-md justify-center flex w-full gap-4 ${
+        mobile && "flex-col h-full"
+      } items-center`}
+    >
+      <li className={`py-2 text-center border-b-4 cursor-pointer`}>
+        <Link href={"/admin"}>Admin</Link>
+      </li>
+      <li className={`py-2 text-center border-b-4 cursor-pointer`}>
+        <Link href={"/user"}>User</Link>
+      </li>
+      <li className={`py-2 text-center border-b-4 cursor-pointer`}>
+        <button>Signout</button>
+      </li>
+      <li className={`py-2 text-center border-b-4 cursor-pointer`}>
+        <button>Signin</button>
+      </li>
+    </ul>
+  );
+};
+
+export default NavItem;
